5717f9e82065f3525c3cf03872ba0d4d9cf29199
Jordan Heale <webmaster@pridit.co.uk>
2024-05-03 13:24:48 +0100

    feat: initial support for map

---
+++ resources/js/Components/Map.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+import 'leaflet/dist/leaflet.css';
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+</script>
+
+<template>
+    <div class="mt-6 h-[24rem]">
+        <l-map :useGlobalLeaflet="false" zoom="4" :center="[0, 0]">
+            <l-tile-layer
+                url="/images/map/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://forums.bohemia.net/forums/topic/101769-takistan-map-high-resolution-14883x14884/" target="_blank">Janne Aartolahti</a>'
+                :tms="true"
+                :min-zoom="3"
+                :max-zoom="6"
+                :noWrap="true"
+            ></l-tile-layer>
+        </l-map>
+    </div>
+</template>
