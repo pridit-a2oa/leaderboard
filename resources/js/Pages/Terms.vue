51f8a4fa140bb45e8b4a25f01714a5dfd1665378
Jordan Heale <webmaster@pridit.co.uk>
2024-05-16 13:45:53 +0100

    feat: iteration

--- resources/js/Pages/Terms.vue
+++ resources/js/Pages/Terms.vue
@@ -43,12 +43,12 @@ import { Head, Link } from '@inertiajs/vue3';
             <p>
                 These Legal Terms constitute a legally binding agreement made
                 between you, whether personally or on behalf of an entity
-                ('you'), and Leaderboard, concerning your access to and use of
-                the Services. You agree that by accessing the Services, you have
-                read, understood, and agreed to be bound by all of these Legal
-                Terms. IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, THEN
-                YOU ARE EXPRESSLY PROHIBITED FROM USING THE SERVICES AND YOU
-                MUST DISCONTINUE USE IMMEDIATELY.
+                ('you'), and Pridit's Domination Takistan, concerning your
+                access to and use of the Services. You agree that by accessing
+                the Services, you have read, understood, and agreed to be bound
+                by all of these Legal Terms. IF YOU DO NOT AGREE WITH ALL OF
+                THESE LEGAL TERMS, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING
+                THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.
             </p>
 
             <p>
