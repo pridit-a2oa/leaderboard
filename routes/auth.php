944780eb31b09a35853de6a2e84f40393a80d4c5
Jordan Heale <webmaster@pridit.co.uk>
2024-05-16 23:32:31 +0100

    feat: add registration throttling

--- routes/auth.php
+++ routes/auth.php
@@ -15,10 +15,12 @@ Route::middleware('guest')->group(function () {
     // Route::get('register', [RegisteredUserController::class, 'create'])
     //             ->name('register');
 
-    // Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register', [RegisteredUserController::class, 'store'])
+        ->middleware('throttle:4,1')
+        ->name('register');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+        ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
@@ -40,20 +42,20 @@ Route::middleware('auth')->group(function () {
     //             ->name('verification.notice');
 
     Route::get('verify-email/{token}', VerifyEmailController::class)
-                ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
 
     // Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
     //             ->middleware('throttle:6,1')
     //             ->name('verification.send');
 
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name('password.confirm');
+        ->name('password.confirm');
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name('logout');
+        ->name('logout');
 });
