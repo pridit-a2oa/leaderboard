0b903fada7163467e87bc536c3f5b106fd164a0f
Jordan Heale <webmaster@pridit.co.uk>
2024-05-30 18:56:54 +0100

    feat: remove unused components, style tweaks to characters page

--- resources/js/Pages/Setting/Characters.vue
+++ resources/js/Pages/Setting/Characters.vue
@@ -23,63 +23,64 @@ import { Head } from '@inertiajs/vue3';
                 message="You have no linked characters"
             />
 
-            <table
-                class="table table-fixed border-separate border-spacing-x-0 border-spacing-y-1 rounded-md bg-base-200"
-            >
-                <tr
-                    v-for="character in $page.props.auth.user.characters.sort(
-                        (a, b) => (a.name > b.name ? 1 : -1),
-                    )"
-                    :key="character.id"
-                >
-                    <td class="w-0">
-                        <FontAwesomeIcon
-                            class="!align-middle"
-                            :icon="faUser"
-                            size="sm"
-                            fixed-width
-                        />
-                    </td>
-
-                    <td
-                        class="truncate pr-2 font-bold text-neutral-500 hover:text-clip hover:whitespace-normal hover:break-all"
+            <table class="table table-fixed rounded-md bg-base-200">
+                <tbody>
+                    <tr
+                        v-for="character in $page.props.auth.user.characters.sort(
+                            (a, b) => (a.name > b.name ? 1 : -1),
+                        )"
+                        :key="character.id"
+                        class="border-base-100 [&:not(:last-child)]:!border-b-4"
                     >
-                        <span class="text-neutral-300">{{
-                            character.name
-                        }}</span>
-                    </td>
-
-                    <td class="text-right">
-                        <div
-                            class="tooltip tooltip-bottom tooltip-secondary before:w-[14rem] before:whitespace-pre-line before:content-[attr(data-tip)]"
-                            data-tip="Toggle whether this character is anonymized in the leaderboard"
-                        >
-                            <VisibilityButton
-                                :id="character.id"
-                                :hidden="character.is_hidden"
+                        <td class="w-0">
+                            <FontAwesomeIcon
+                                class="!align-middle"
+                                :icon="faUser"
+                                size="sm"
+                                fixed-width
                             />
-                        </div>
+                        </td>
 
-                        <div
-                            class="tooltip tooltip-bottom tooltip-warning ml-3 before:w-[17rem] before:whitespace-pre-line before:content-[attr(data-tip)]"
-                            data-tip="Unlink this character from your account (can be relinked at any time)"
+                        <td
+                            class="truncate pr-2 font-bold text-neutral-500 hover:text-clip hover:whitespace-normal hover:break-all"
                         >
-                            <UnlinkButton :id="character.id" />
-                        </div>
+                            <span class="text-neutral-300">{{
+                                character.name
+                            }}</span>
+                        </td>
 
-                        <div
-                            v-if="
-                                $page.props.roles.some((role) =>
-                                    ['admin', 'supporter'].includes(role),
-                                ) && character.score > 0
-                            "
-                            class="tooltip tooltip-bottom tooltip-error ml-3 before:w-[13rem] before:whitespace-pre-line before:content-[attr(data-tip)]"
-                            data-tip="Reset score and any additional statistics for this character&#10;(cannot be reversed)"
-                        >
-                            <ResetButton :id="character.id" />
-                        </div>
-                    </td>
-                </tr>
+                        <td class="text-right">
+                            <div
+                                class="tooltip tooltip-bottom tooltip-secondary before:w-[14rem] before:whitespace-pre-line before:content-[attr(data-tip)]"
+                                data-tip="Toggle whether this character is anonymized in the leaderboard"
+                            >
+                                <VisibilityButton
+                                    :id="character.id"
+                                    :hidden="character.is_hidden"
+                                />
+                            </div>
+
+                            <div
+                                class="tooltip tooltip-bottom tooltip-warning ml-3 before:w-[17rem] before:whitespace-pre-line before:content-[attr(data-tip)]"
+                                data-tip="Unlink this character from your account (can be relinked at any time)"
+                            >
+                                <UnlinkButton :id="character.id" />
+                            </div>
+
+                            <div
+                                v-if="
+                                    $page.props.roles.some((role) =>
+                                        ['admin', 'supporter'].includes(role),
+                                    ) && character.score > 0
+                                "
+                                class="tooltip tooltip-bottom tooltip-error ml-3 before:w-[13rem] before:whitespace-pre-line before:content-[attr(data-tip)]"
+                                data-tip="Reset score and any additional statistics for this character&#10;(cannot be reversed)"
+                            >
+                                <ResetButton :id="character.id" />
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </Setting>
     </DefaultLayout>
