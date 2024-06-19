82e035c3238b96e0a7b64be0e12127e3bbb1020c
Jordan Heale <webmaster@pridit.co.uk>
2024-06-19 14:41:59 +0100

    feat: add webp logo

--- resources/js/Components/ui/Banner.vue
+++ resources/js/Components/ui/Banner.vue
@@ -7,11 +7,12 @@ import { Link } from '@inertiajs/vue3';
         <div class="navbar justify-center bg-base-200 py-6 text-neutral-300">
             <h1 class="text-xl">
                 <Link class="inline-flex items-center" :href="route('home')">
-                    <img
-                        class="mr-4 h-10 select-none"
-                        src="/images/logo.png"
-                        alt="Logo"
-                    />
+                    <picture
+                        class="mr-4 h-10 w-10 select-none rounded-full bg-base-100"
+                    >
+                        <source type="image/webp" srcset="/images/logo.webp" />
+                        <img src="/images/logo.png" alt="Logo" loading="lazy" />
+                    </picture>
 
                     {{ $page.props.app.name }}
                 </Link>
