7660e4f100b899bfba8dd6bddd8b475f2d234afa
Jordan Heale <webmaster@pridit.co.uk>
2024-06-19 03:52:57 +0100

    fix: change reward icon state dependent on user

--- resources/js/Components/features/reward/RewardIcon.vue
+++ resources/js/Components/features/reward/RewardIcon.vue
@@ -9,21 +9,18 @@ import {
     FontAwesomeIcon,
     FontAwesomeLayers,
 } from '@fortawesome/vue-fontawesome';
-import { usePage } from '@inertiajs/vue3';
 
-defineProps({
+const props = defineProps({
     user: {
         type: Object,
-    },
-    size: {
-        type: String,
+        required: true,
     },
 });
 
 const rewarded = computed(() => {
     return (
-        usePage().props.auth.user.connections.length !== 0 &&
-        usePage().props.auth.user.characters.length !== 0
+        props.user.connections.length !== 0 &&
+        props.user.characters.length !== 0
     );
 });
 </script>
@@ -32,7 +29,7 @@ const rewarded = computed(() => {
     <FontAwesomeLayers class="indicator mr-1">
         <FontAwesomeIcon :icon="faPersonRifle" fixed-width />
         <FontAwesomeIcon
-            v-if="user !== null"
+            v-show="user !== null"
             :icon="rewarded ? faCircleCheck : faCircleXmark"
             class="indicator-item indicator-end indicator-bottom rounded-full bg-base-200 p-0 py-0.5"
             :class="rewarded ? 'text-green-700' : 'text-red-500'"
