185bebac8873c0e81fc2ab4c73cefa420b0e37ae
Jordan Heale <webmaster@pridit.co.uk>
2024-10-13 13:02:31 +0100

    chore: sort app imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const colors = require('tailwindcss/colors');
 
