61439b2589b654182bad0bb342b3ab70e0605f3b
Jordan Heale <webmaster@pridit.co.uk>
2025-05-03 12:30:45 +0100

    feat: add consistent number width to suffix

--- resources/js/Components/text/SuffixText.vue
+++ resources/js/Components/text/SuffixText.vue
@@ -7,7 +7,7 @@ defineProps({
 <template>
     <span
         v-if="value && value !== '0'"
-        class="bg-base-100 rounded px-1.5 text-xs outline outline-1 outline-neutral-500 outline-solid"
+        class="bg-base-100 rounded px-1.5 text-xs tabular-nums outline outline-1 outline-neutral-500 outline-solid"
         >{{ value }}</span
     >
 </template>
